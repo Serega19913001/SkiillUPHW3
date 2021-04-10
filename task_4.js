1675031cc5f77dd801b41fb89a946a081d7de1db
Serega19913001 <serlevchenko@ukr.net>
2021-04-10 22:24:19 +0300

    homework

---
+++ task_4.js
@@ -0,0 +1,37 @@
+/* Задание 4
+Напиши функцию countTotalSalary(employees) принимающую объект зарплат. Функция считает общую сумму зарплаты работников и 
+возращает ее. Каждое поле объекта, передаваемого в функцию, имеет вид "имя":"зарплата".
+const countTotalSalary = function(employees) {
+  // твой код
+}; */
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+
+const countTotalSalary = function(employees) {
+  
+  let key = 0;
+  for( let i in Object.values (employees)){
+    key +=i
+  }
+  return key
+};
+
+
+
+
+console.log(countTotalSalary({})); // 0
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+  }),
+); // 330
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+  }),
+); // 400
